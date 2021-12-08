3157400873412b1f95ef8f0e05ca30f2ed306125
LucasG0ld <72749416+LucasG0ld@users.noreply.github.com>
2021-12-08 12:34:05 +0100

    Responsive & modal done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,17 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const formClose = document.querySelectorAll(".close");
+const firstName = document.getElementById('first').value;
+const lastName = document.getElementById('last').value;
+const email = document.getElementById('email').value;
+const birthDate = document.getElementById('birthdate').value;
+const tournamentQuantity = document.getElementById('quantity').value;
+const location1 = document.getElementById('location1').value;
+const location2 = document.getElementById('location2').value;
+const location3 = document.getElementById('location3').value;
+const location4 = document.getElementById('location4').value;
+const location5 = document.getElementById('location5').value;
+const location6 = document.getElementById('location6').value;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,3 +36,5 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+
