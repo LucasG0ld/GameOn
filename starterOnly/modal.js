d4f2c2dd4ca2f82dc18f1e25a8e6ee5b6d4619d3
LucasG0ld <72749416+LucasG0ld@users.noreply.github.com>
2021-12-13 19:45:18 +0100

    Form done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const formClose = document.querySelectorAll(".close");
+const btnThanks =  document.getElementById("btn-thanks");
+let gameThanks = document.getElementById('game-thanks');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,8 +30,23 @@ function closeModal() {
   modalbg.classList.remove("dp-block");
 }
 
+// close thanks modal on the button
+btnThanks.addEventListener("click", function closeThanks() {
+  gameThanks.classList.add("dp-none");
+  gameThanks.classList.remove("dp-block");
+});
+
+// close modal form on the cross
+function closeThanks() {
+  gameThanks.classList.add("dp-none");
+  gameThanks.classList.remove("dp-block");
+}
+
+
 // Validation form
-document.getElementById("gameForm").addEventListener("submit", function(e) {
+document.getElementById("gameForm").addEventListener("submit", function validateForm(e) {
+  let gameForm = document.getElementById('gameForm');
+  let formBground = document.getElementById('formBground');
   let firstName = document.getElementById('first').value;
   let textFirst = document.getElementById('text-first');
   let lastName = document.getElementById('last').value;
@@ -50,46 +67,74 @@ document.getElementById("gameForm").addEventListener("submit", function(e) {
   let condition = document.getElementById('checkbox1');
   let textCondition = document.getElementById('text-condition');
   
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let regexName = /^[a-zA-Z ]{2,30}$/
 
-  if (firstName.length <= 2) {
+  e.preventDefault();
+
+  if (firstName.length <= 2 || !firstName.match(regexName)) {
     textFirst.classList.add("dp-block");
     textFirst.classList.remove("dp-none");
     e.preventDefault();
+    return false;
+  } else {
+    textFirst.classList.add("dp-none");
   }
-  if (lastName.length <= 2) {
+  if (lastName.length <= 2 || !lastName.match(regexName)) {
     textLast.classList.add("dp-block");
     textLast.classList.remove("dp-none");
     e.preventDefault();
+    return false;
+  } else {
+    textLast.classList.add("dp-none");
   }
-  if (email == "") {
+  if (email == "" || !email.match(regexEmail)) {
     textEmail.classList.add("dp-block");
     textEmail.classList.remove("dp-none");
     e.preventDefault();
-  }
-  if (email.indexOf("@", 0) < 0) {
-    textEmail.classList.add("dp-block");
-    textEmail.classList.remove("dp-none");
-    e.preventDefault();
-  }
-  if (email.indexOf(".", 0) < 0) {
-    textEmail.classList.add("dp-block");
-    textEmail.classList.remove("dp-none");
-    e.preventDefault();
+    return false;
+  } else {
+    textEmail.classList.add("dp-none");
   }
   if (birthDate == "") {
     textBirth.classList.add("dp-block");
     textBirth.classList.remove("dp-none");
+    e.preventDefault();
+    return false;
+  } else {
+    textBirth.classList.add("dp-none");
   }
   if (tournamentQtt == "") {
     textQtt.classList.add("dp-block");
     textQtt.classList.remove("dp-none");
+    e.preventDefault();
+    return false;
+  } else {
+    textQtt.classList.add("dp-none");
   }
   if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) {
     textLocation.classList.add("dp-block");
     textLocation.classList.remove("dp-none");
+    e.preventDefault();
+    return false;
+  } else {
+    textLocation.classList.add("dp-none");
   }
   if (!condition.checked) {
     textCondition.classList.add("dp-block");
     textCondition.classList.remove("dp-none");
+    e.preventDefault();
+    return false;
+  } else {
+    textCondition.classList.add("dp-none");
+  }
+  if (gameForm.submit) {
+    gameThanks.classList.add("dp-block");
+    gameThanks.classList.remove("dp-none");
+    formBground.classList.remove("dp-block");
+    formBground.classList.add("dp-none");
+    e.preventDefault();
+    return false;
+    
   }
 });
